85fe61275731adee421d8cd20091714e60b21fca
tarsisius <zkripsi@gmail.com>
2017-03-26 03:34:06 -0700

    Update zkrip.js

--- zkrip.js
+++ zkrip.js
@@ -34,11 +34,3 @@ eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
 //<![CDATA[
 var ww=document.body.clientWidth;$(document).ready(function(){$(".nav li a").each(function(){if($(this).next().length>0){$(this).addClass("parent")}});$(".push").click(function(e){e.preventDefault();$(this).toggleClass("active");$(".nav").toggle()});adjustMenu()});$(window).bind("resize orientationchange",function(){ww=document.body.clientWidth;adjustMenu()});var adjustMenu=function(){if(ww<768){$(".push").css("display","inline-block");if(!$(".push").hasClass("active")){$(".nav").hide()}else{$(".nav").show()}$(".nav li").unbind("mouseenter mouseleave");$(".nav li a.parent").unbind("click").bind("click",function(e){e.preventDefault();$(this).parent("li").toggleClass("hover")})}else if(ww>=768){$(".push").css("display","none");$(".nav").show();$(".nav li").removeClass("hover");$(".nav li a").unbind("click");$(".nav li").unbind("mouseenter mouseleave").bind("mouseenter mouseleave",function(){$(this).toggleClass("hover")})}}
 //]]>
-var pres = document.getElementsByTagName(&quot;blockquote&quot;);
-for (var i = 0; i &lt; pres.length; i++) {
-  pres[i].addEventListener(&quot;dblclick&quot;, function () {
-    var selection = getSelection();
-    var range = document.createRange();
-    range.selectNodeContents(this);
-    selection.removeAllRanges();
-    selection.addRange(range);   }, false); }
